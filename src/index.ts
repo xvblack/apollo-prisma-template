832726b09e2a5bdc88bffa42d5057edd5070e566
X Etrev <xvblue@gmail.com>
2022-11-22 14:37:07 +0800

    add subscription

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
+import { createServer } from "http";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
 import { loadSchema } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { PrismaClient } from "@prisma/client";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import express from "express";
+import cors from "cors";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+const prisma = new PrismaClient();
+
 const typeDefs = await loadSchema("./graphql/schema.graphql", {
   loaders: [new GraphQLFileLoader()],
 });
-
-const prisma = new PrismaClient();
 
 const resolvers = {
   Query: {
@@ -22,6 +26,17 @@ const resolvers = {
     },
     posts: async () => {
       return prisma.post.findMany();
+    },
+  },
+  Subscription: {
+    welcome: {
+      // Example using an async generator
+      subscribe: async function* () {
+        for await (const word of ["Hello", "Bonjour", "Ciao"]) {
+          await new Promise((r) => setTimeout(r, 2000));
+          yield { welcome: { content: word } };
+        }
+      },
     },
   },
   User: {
@@ -44,19 +59,59 @@ const resolvers = {
   },
 };
 
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
+  // plugins: [
+  //   // Proper shutdown for the HTTP server.
+  //   ApolloServerPluginDrainHttpServer({ httpServer }),
+
+  //   // Proper shutdown for the WebSocket server.
+  //   {
+  //     async serverWillStart() {
+  //       return {
+  //         async drainServer() {
+  //           await serverCleanup.dispose();
+  //         },
+  //       };
+  //     },
+  //   },
+  // ],
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+await server.start();
+
+const app = express();
+
+// Specify the path where we'd like to mount our server
+app.use(
+  "/graphql",
+  cors<cors.CorsRequest>(),
+  express.json(),
+  expressMiddleware(server)
+);
+
+// This `app` is the returned value from `express()`.
+const httpServer = createServer(app);
+
+// Creating the WebSocket server
+const wsServer = new WebSocketServer({
+  // This is the `httpServer` we created in a previous step.
+  server: httpServer,
+  // Pass a different path here if app.use
+  // serves expressMiddleware at a different path
+  path: "/graphql",
 });
 
-console.log(`🚀  Server ready at: ${url}`);
+// Hand in the schema we just created and have the
+// WebSocketServer start listening.
+const serverCleanup = useServer({ schema }, wsServer);
+
+// Modified server startup
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve)
+);
+console.log(`🚀 Server ready at http://localhost:4000/`);
